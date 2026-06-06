ffe08c6af791469be1428392b58f8e21c81f559f
kunalbubna263 <kunalbubna9@gmail.com>
2026-06-06 12:25:58 +0530

    added interview question-feature-SDE questions only

---
+++ EmpleFrontend/src/app/user/resources/interview-questions/cn/page.tsx
@@ -0,0 +1,5 @@
+import CnQuestionsPage from "@/features/user/resources/pages/interview-questions/CnQuestionsPage";
+
+export default function Page() {
+  return <CnQuestionsPage />;
+}
---
+++ EmpleFrontend/src/app/user/resources/interview-questions/devops/page.tsx
@@ -0,0 +1,5 @@
+import DevopsQuestionsPage from "@/features/user/resources/pages/interview-questions/DevopsQuestionsPage";
+
+export default function Page() {
+  return <DevopsQuestionsPage />;
+}
