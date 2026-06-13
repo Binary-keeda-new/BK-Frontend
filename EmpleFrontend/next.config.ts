b90e590798491207aabd0a1e02c22243cad66dbd
Lakshita <gungundeopura678@gmail.com>
2026-06-13 14:19:36 +0530

    Fix:Syntax error in next.config.ts and ResourcesHome.tsx

--- EmpleFrontend/next.config.ts
+++ EmpleFrontend/next.config.ts
@@ -1,8 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
   output: 'standalone',
-};
-module.exports = nextConfig;
   images: {
     domains: ["upload.wikimedia.org"],
   },
