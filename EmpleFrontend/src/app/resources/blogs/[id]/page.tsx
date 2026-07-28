7ee4aa460443c69e67e3e04febae3c4118a3fe1f
Lakshita <gungundeopura678@gmail.com>
2026-07-28 11:20:26 +0530

    Blogs page issue resolved

--- EmpleFrontend/src/app/resources/blogs/[id]/page.tsx
+++ EmpleFrontend/src/app/resources/blogs/[id]/page.tsx
@@ -1,8 +1,8 @@
 import BlogDetailPage from "@/features/user/blog/pages/BlogDetailPage";
 
 // Minimal dynamic metadata generation for SEO
-export async function generateMetadata({ params }: { params: { id: string } }) {
-  const id = params.id;
+export async function generateMetadata({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
   const title = id.split('-').map(w => w.charAt(0).toUpperCase() + w.slice(1)).join(' ');
   return {
     title: `${title} | Emple Blogs`,
